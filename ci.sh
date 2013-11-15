f5f05d174578d3db92685ec1e38a32aa10a56797
Christoph Scheid <c@shri.de>
2013-11-15 03:36:34 +0100

    update ci script

--- ci.sh
+++ ci.sh
@@ -20,6 +20,12 @@ env/bin/pip install nose coverage pep8 pylint pyflakes
 # customized TextInputSliderWidget needs to be copied in place
 cp  deform/widget.py env/lib/python2.7/site-packages/deform-0.9.9-py2.7.egg/deform/widget.py
 cp  deform/slider.pt env/lib/python2.7/site-packages/deform-0.9.9-py2.7.egg/deform/templates/slider.pt
+#
+# test preparation
+#
+# we use selenium for user interface tests. so we need firefox and xvfb
+# start Xvfband send it to the background: Xvfb :10 &
+export DISPLAY=:10
 # run the tests
 env/bin/nosetests c3smembership/   --with-coverage --cover-html --with-xunit
 # this is how you can run individial tests:
