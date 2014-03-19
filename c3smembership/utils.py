054aa2eab9357acf8cdb4ad1ef75b947c1d33c1d
Christoph Scheid <c@shri.de>
2014-03-19 13:16:53 +0100

    change/announce C3S office address

--- c3smembership/utils.py
+++ c3smembership/utils.py
@@ -30,9 +30,9 @@ def generate_pdf(appstruct):
     import os
     here = os.path.dirname(__file__)
     declaration_pdf_de = os.path.join(
-        here, "../pdftk/C3S-SCE-AFM-v06-20140105-de.pdf")
+        here, "../pdftk/C3S-SCE-AFM-v07-20140314-de.pdf")
     declaration_pdf_en = os.path.join(
-        here, "../pdftk/C3S-SCE-AFM-v06-20140105-en.pdf")
+        here, "../pdftk/C3S-SCE-AFM-v07-20140314-en.pdf")
 
     # check for _LOCALE_, decide which language to use
     #print(appstruct['_LOCALE_'])
