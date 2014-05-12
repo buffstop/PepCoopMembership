d0f2d38c93e1b16eddb22b08501214e988fa0035
Christoph Scheid <c@shri.de>
2014-05-12 11:02:24 +0200

    let accountants add new members

---
+++ c3smembership/new_member.py
@@ -0,0 +1,373 @@
+# -*- coding: utf-8 -*-
+
+from c3smembership.models import (
+    C3sMember,
+    DBSession,
+)
+from c3smembership.utils import (
+    _,
+    country_codes,
+)
+
+import colander
+from colander import (
+    Invalid,
+    Range,
+)
+from datetime import (
+    date,
+    datetime,
+)
+import deform
+from deform import ValidationFailure
+
+from pyramid.httpexceptions import HTTPFound
+from pyramid.view import view_config
+from sqlalchemy.exc import (
+    InvalidRequestError,
+    IntegrityError
+)
+from types import NoneType
+
+country_default = 'Germany'
+
+
+@view_config(route_name="new_member",
+             permission="manage",
+             renderer="templates/new_member.pt")
+def new_member(request):
+    '''
+    let staff create a new member entry, when receiving input via dead wood
+    '''
+
+    # XXX check if submitted, etc...
+
+    class PersonalData(colander.MappingSchema):
+        """
+        colander schema for membership application form
+        """
+        #id = colander.SchemaNode(
+        #    colander.Integer(),
+        #    title='Database ID (optional, used to re-add deleted member',
+        #)
+        firstname = colander.SchemaNode(
+            colander.String(),
+            title='Vorname',
+            oid="firstname",
+        )
+        lastname = colander.SchemaNode(
+            colander.String(),
+            title='Nachnahme',
+            oid="lastname",
+        )
+        email = colander.SchemaNode(
+            colander.String(),
+            title=_(u'Email'),
+            validator=colander.Email(),
+            oid="email",
+        )
+        passwort = colander.SchemaNode(
+            colander.String(),
+            widget=deform.widget.HiddenWidget(),
+            default='NoneSet',
+            missing='NoneSetPurposefully'
+        )
+        address1 = colander.SchemaNode(
+            colander.String(),
+            title='Addresse Zeile 1'
+        )
+        address2 = colander.SchemaNode(
+            colander.String(),
+            missing=unicode(''),
+            title='Addresse Zeile 2'
+        )
+        postcode = colander.SchemaNode(
+            colander.String(),
+            title='Postleitzahl',
+            oid="postcode"
+        )
+        city = colander.SchemaNode(
+            colander.String(),
+            title='Ort',
+            oid="city",
+        )
+        country = colander.SchemaNode(
+            colander.String(),
+            title='Land',
+            default=country_default,
+            widget=deform.widget.SelectWidget(
+                values=country_codes),
+            oid="country",
+        )
+        date_of_birth = colander.SchemaNode(
+            colander.Date(),
+            title='Geburtsdatum',
+            #widget=deform.widget.DatePartsWidget(
+            #    inline=True),
+            default=date(2013, 1, 1),
+            validator=Range(
+                min=date(1913, 1, 1),
+                max=date(2000, 1, 1),
+                min_err=_(u'${val} is earlier than earliest date ${min}'),
+                max_err=_(u'${val} is later than latest date ${max}')
+            ),
+            oid="date_of_birth",
+        )
+        _LOCALE_ = colander.SchemaNode(
+            colander.String(),
+            widget=deform.widget.HiddenWidget(),
+            default='DE',
+            missing='DE',
+        )
+
+    class MembershipInfo(colander.Schema):
+
+        yes_no = ((u'yes', _(u'Yes')),
+                  (u'no', _(u'No')))
+
+        membership_type = colander.SchemaNode(
+            colander.String(),
+            title=(u'Art der Mitgliedschaft (lt. Satzung, §4)'),
+            description=u'Bitte die Art der Mitgliedschaft auswählen.',
+            widget=deform.widget.RadioChoiceWidget(
+                values=(
+                    (u'normal',
+                     (u'Normales Mitglied')),
+                    (u'investing',
+                     u'Investierendes Mitglied'),
+                ),
+            )
+        )
+        member_of_colsoc = colander.SchemaNode(
+            colander.String(),
+            title='Mitglied einer Verwertungsgesellschaft?',
+            validator=colander.OneOf([x[0] for x in yes_no]),
+            widget=deform.widget.RadioChoiceWidget(values=yes_no),
+            oid="other_colsoc",
+            #validator=colsoc_validator
+        )
+        name_of_colsoc = colander.SchemaNode(
+            colander.String(),
+            title=(u'Falls ja, welche? (Kommasepariert)'),
+            missing=unicode(''),
+            oid="colsoc_name",
+            #validator=colander.All(
+            #    colsoc_validator,
+            #)
+        )
+
+    class Shares(colander.Schema):
+        """
+        the number of shares a member wants to hold
+
+        this involves a slider widget: added to deforms widgets.
+        see README.Slider.rst
+        """
+        num_shares = colander.SchemaNode(
+            colander.Integer(),
+            title='Anzahl Anteile (1-60)',
+            default="1",
+            #widget=deform.widget.TextInputSliderWidget(
+            #    size=3, css_class='num_shares_input'),
+            validator=colander.Range(
+                min=1,
+                max=60,
+                min_err=u'mindestens 1',
+                max_err=u'höchstens 60',
+            ),
+            oid="num_shares")
+
+    class MembershipForm(colander.Schema):
+        """
+        The Form consists of
+        - Personal Data
+        - Membership Information
+        - Shares
+        """
+        person = PersonalData(
+            title=_(u"Personal Data"),
+            #description=_(u"this is a test"),
+            #css_class="thisisjustatest"
+        )
+        membership_info = MembershipInfo(
+            title=_(u"Membership Requirements")
+        )
+        shares = Shares(
+            title=_(u"Shares")
+        )
+
+    schema = MembershipForm()
+
+    form = deform.Form(
+        schema,
+        buttons=[
+            deform.Button('submit', _(u'Submit')),
+            deform.Button('reset', _(u'Reset'))
+        ],
+        use_ajax=True,
+        #renderer=zpt_renderer
+    )
+
+    # if the form has NOT been used and submitted, remove error messages if any
+    if not 'submit' in request.POST:
+        request.session.pop_flash()
+        #print('ping!')
+
+    # if the form has been used and SUBMITTED, check contents
+    if 'submit' in request.POST:
+        controls = request.POST.items()
+        try:
+            appstruct = form.validate(controls)
+            #print("the appstruct from the form: %s \n") % appstruct
+            #for thing in appstruct:
+            #    print("the thing: %s") % thing
+            #    print("type: %s") % type(thing)
+
+            # data sanity: if not in collecting society, don't save
+            #  collsoc name even if it was supplied through form
+            #if 'no' in appstruct['membership_info']['member_of_colsoc']:
+            #    appstruct['membership_info']['name_of_colsoc'] = ''
+            #    print appstruct['membership_info']['name_of_colsoc']
+            #print '-'*80
+
+        except ValidationFailure as e:
+            #print("Validation Failure!")
+            #print("the request.POST: %s \n" % request.POST)
+            #for thing in request.POST:
+            #    print("the thing: %s") % thing
+            #    print("type: %s") % type(thing)
+            #print(e.args)
+            #print(e.error)
+            #print(e.message)
+            request.session.flash(
+                _(u"Please note: There were errors, "
+                  "please check the form below."),
+                'message_above_form',
+                allow_duplicate=False)
+            return{'form': e.render()}
+
+        def make_random_string():
+            """
+            used as email confirmation code
+            """
+            import random
+            import string
+            return u''.join(
+                random.choice(
+                    string.ascii_uppercase + string.digits
+                ) for x in range(10))
+
+        # make confirmation code and
+        randomstring = make_random_string()
+        # check if confirmation code is already used
+        while (C3sMember.check_for_existing_confirm_code(randomstring)):
+            # create a new one, if the new one already exists in the database
+            randomstring = make_random_string()  # pragma: no cover
+
+        # to store the data in the DB, an objet is created
+        member = C3sMember(
+            firstname=appstruct['person']['firstname'],
+            lastname=appstruct['person']['lastname'],
+            email=appstruct['person']['email'],
+            password='UNSET',
+            address1=appstruct['person']['address1'],
+            address2=appstruct['person']['address2'],
+            postcode=appstruct['person']['postcode'],
+            city=appstruct['person']['city'],
+            country=appstruct['person']['country'],
+            locale=appstruct['person']['_LOCALE_'],
+            date_of_birth=appstruct['person']['date_of_birth'],
+            email_is_confirmed=False,
+            email_confirm_code=randomstring,
+            #is_composer=('composer' in appstruct['activity']),
+            #is_lyricist=('lyricist' in appstruct['activity']),
+            #is_producer=('music producer' in appstruct['activity']),
+            #is_remixer=('remixer' in appstruct['activity']),
+            #is_dj=('dj' in appstruct['activity']),
+            date_of_submission=datetime.now(),
+            #invest_member=(
+            #    appstruct['membership_info']['invest_member'] == u'yes'),
+            membership_type=appstruct['membership_info']['membership_type'],
+            member_of_colsoc=(
+                appstruct['membership_info']['member_of_colsoc'] == u'yes'),
+            name_of_colsoc=appstruct['membership_info']['name_of_colsoc'],
+            #opt_band=appstruct['opt_band'],
+            #opt_URL=appstruct['opt_URL'],
+            num_shares=appstruct['shares']['num_shares'],
+        )
+        dbsession = DBSession()
+
+        try:
+            _temp = request.url.split('?')[1].split('=')
+            if 'id' in _temp[0]:
+                _id = _temp[1]
+                #print("the id we want to recreate: %s" % _id)
+
+            # add a member with a DB id that had seen its entry deleted before
+                _mem = C3sMember.get_by_id(_id)  # load from id
+                if isinstance(_mem, NoneType):  # check deletion status
+                    member.id = _id  # set id as specified
+        except:
+            #print "no splitable url params found, creating new entry"
+            pass
+
+        # add member at next free DB id (default if member.id not set)
+        try:
+            dbsession.add(member)
+            dbsession.flush()
+            #print(member.id)
+            the_new_id = member.id
+            #appstruct['email_confirm_code'] = randomstring  # ???
+        except InvalidRequestError, e:  # pragma: no cover
+            print("InvalidRequestError! %s") % e
+        except IntegrityError, ie:  # pragma: no cover
+            print("IntegrityError! %s") % ie
+
+        # send mail to accountants // prepare a mailer
+        #mailer = get_mailer(request)
+        # prepare mail
+        #the_mail = accountant_mail(appstruct)
+        #mailer.send(the_mail)
+        #log.info("NOT sending mail...")
+
+        #return generate_pdf(appstruct)  # would just return a PDF
+
+        # redirect to success page, then return the PDF
+        # first, store appstruct in session
+        request.session['appstruct'] = appstruct
+        request.session[
+            'appstruct']['_LOCALE_'] = appstruct['person']['_LOCALE_']
+        #from pyramid.httpexceptions import HTTPFound
+        #
+        # empty the messages queue (as validation worked anyways)
+        deleted_msg = request.session.pop_flash()
+        del deleted_msg
+        return HTTPFound(  # redirect to success page
+            location=request.route_url(
+                'detail',
+                memberid=the_new_id),
+        )
+
+    # if the form was submitted and gathered info shown on the success page,
+    # BUT the user wants to correct their information:
+    else:
+        # remove annoying message from other session
+        deleted_msg = request.session.pop_flash()
+        del deleted_msg
+        if ('appstruct' in request.session):
+            #print("form was not submitted, but found appstruct in session.")
+            appstruct = request.session['appstruct']
+            #print("the appstruct: %s") % appstruct
+            # pre-fill the form with the values from last time
+            form.set_appstruct(appstruct)
+            #import pdb
+            #pdb.set_trace()
+            #form = deform.Form(schema,
+            #           buttons=[deform.Button('submit', _(u'Submit'))],
+            #           use_ajax=True,
+            #           renderer=zpt_renderer
+            #           )
+
+    html = form.render()
+
+    return {'form': html}
---
+++ c3smembership/tests/test_new_member_webtest.py
@@ -0,0 +1,275 @@
+#!/bin/env/python
+# -*- coding: utf-8 -*-
+
+import unittest
+from pyramid import testing
+
+from c3smembership.models import (
+    DBSession,
+    Base,
+    C3sMember,
+    C3sStaff,
+    Group,
+)
+from sqlalchemy import engine_from_config
+import transaction
+from datetime import date
+
+
+class NewMemberTests(unittest.TestCase):
+    """
+    test creation of a new member by staff
+
+    these tests are functional tests to check functionality of the whole app
+    (i.e. integration tests)
+    they also serve to get coverage for 'main'
+    """
+    def setUp(self):
+        self.config = testing.setUp()
+        self.config.include('pyramid_mailer.testing')
+        try:
+            DBSession.close()
+            DBSession.remove()
+            #print "closed and removed DBSession"
+        except:
+            pass
+            #print "no session to close"
+       # self.session = DBSession()
+        my_settings = {
+            'sqlalchemy.url': 'sqlite:///:memory:',
+            'available_languages': 'da de en es fr',
+            'c3smembership.dashboard_number': '30'}
+        engine = engine_from_config(my_settings)
+        DBSession.configure(bind=engine)
+        Base.metadata.create_all(engine)
+
+        with transaction.manager:
+            # a group for accountants/staff
+            accountants_group = Group(name=u"staff")
+            try:
+                DBSession.add(accountants_group)
+                DBSession.flush()
+            except:
+                pass
+            # staff personnel
+            staffer1 = C3sStaff(
+                login=u"rut",
+                password=u"berries",
+                email=u"noreply@c3s.cc",
+            )
+            staffer1.groups = [accountants_group]
+            try:
+                DBSession.add(accountants_group)
+                DBSession.add(staffer1)
+                DBSession.flush()
+            except:
+                #print("it borked! (rut)")
+                pass
+
+        from c3smembership import main
+        app = main({}, **my_settings)
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def tearDown(self):
+        DBSession.close()
+        DBSession.remove()
+        testing.tearDown()
+
+    def test_add_member(self):
+        '''
+        tests for the new_member view
+        '''
+        # unauthorized access must be prevented
+        res = self.testapp.reset()  # delete cookie
+        res = self.testapp.get('/new_member', status=403)
+        assert('Access was denied to this resource' in res.body)
+        # so login first
+        res = self.testapp.get('/login', status=200)
+        self.failUnless('login' in res.body)
+        # try valid user
+        form = res.form
+        form['login'] = 'rut'
+        form['password'] = 'berries'
+        res2 = form.submit('submit', status=302)
+        # # being logged in ...
+        res3 = res2.follow()  # being redirected to dashboard_only
+        #print('>'*20)
+        #print(res3.body)
+        #print('<'*20)
+        res4 = res3.follow()  # being redirected to dashboard with parameters
+        self.failUnless(
+            'Dashboard' in res4.body)
+        # # now that we are logged in,
+        # # the login view should redirect us to the dashboard
+        # res5 = self.testapp.get('/login', status=302)
+        # # so yes: that was a redirect
+        # res6 = res5.follow()
+        # res6 = res6.follow()
+        # #print(res4.body)
+        # self.failUnless(
+        #     'Dashboard' in res6.body)
+        # # choose number of applications shown
+        # res6a = self.testapp.get(
+        #     '/dashboard',
+        #     status=302,
+        #     extra_environ={
+        #         'num_display': '30',
+        #     }
+        # )
+        # res6a = res6a.follow()
+
+        # no member with id=1 in DB
+        res = self.testapp.get('/new_member?id=1', status=200)
+
+        form = res.form
+        #print form.fields
+        form['firstname'] = u'SomeFirstname',
+        form['lastname'] = u'SomeLastname',
+        form['email'] = u'some@shri.de',
+        form['address1'] = u"addr one",
+        form['address2'] = u"addr two",
+        form['postcode'] = u"12345",
+        form['city'] = u"Footown Meeh",
+        form['country'].value__set(u"DE"),
+        form['_LOCALE_'] = u"DE",
+        form['date_of_birth'] = '1014-01-01'  # date.today(),
+        #form['email_is_confirmed=False,
+        #email_confirm_code=u'ABCDEFGFOO',
+        #password=u'arandompassword',
+        #date_of_submission=date.today(),
+        #form['membership_type'] = u'normal',
+        form['deformField14'].value__set(u'normal')
+        #form['other_colsoc'] = (u'no',),
+        form['other_colsoc'].value__set(u'no')
+
+        form['name_of_colsoc'] = u"GEMA",
+        form['num_shares'] = u'23',
+
+        # try to submit the form (knowing that the date is missing)
+        res2 = form.submit(u'submit', status=200)
+        form2 = res2.form
+        # set the date to a usefull/allowed value
+        form2['date_of_birth'] = u'1999-09-19'
+        res3 = form2.submit(u'submit', status=302)
+        res4 = res3.follow()
+        #print res4
+        self.assertTrue('Details for Member Application #1' in res4.body)
+
+        # more asserts
+        self.assertTrue('SomeFirstname' in res4.body)
+        self.assertTrue('SomeLastname' in res4.body)
+        self.assertTrue('some@shri.de' in res4.body)
+        self.assertTrue('addr one' in res4.body)
+        self.assertTrue('addr two' in res4.body)
+        self.assertTrue('12345' in res4.body)
+        self.assertTrue('' in res4.body)
+        self.assertTrue('DE' in res4.body)
+        self.assertTrue('normal' in res4.body)
+        self.assertTrue('23' in res4.body)
+        #        self.assertTrue('' in res3.body)
+        #        self.assertTrue('' in res3.body)
+        #        self.assertTrue('' in res3.body)
+        #        self.assertTrue('' in res3.body)
+
+        # now, there is a member with id=1 in DB
+        res = self.testapp.get('/new_member?id=1', status=200)
+
+        form = res.form
+        #print form.fields
+        form['firstname'] = u'SomeFirstname',
+        form['lastname'] = u'SomeLastname',
+        form['email'] = u'some@shri.de',
+        form['address1'] = u"addr one",
+        form['address2'] = u"addr two",
+        form['postcode'] = u"12345",
+        form['city'] = u"Footown Meeh",
+        form['country'].value__set(u"DE"),
+        form['_LOCALE_'] = u"DE",
+        form['date_of_birth'] = date.today(),
+        #form['email_is_confirmed=False,
+        #email_confirm_code=u'ABCDEFGFOO',
+        #password=u'arandompassword',
+        #date_of_submission=date.today(),
+        #form['membership_type'] = u'normal',
+        form['deformField14'].value__set(u'normal')
+        #form['other_colsoc'] = (u'no',),
+        form['other_colsoc'].value__set(u'no')
+
+        form['name_of_colsoc'] = u"GEMA",
+        form['num_shares'] = u'23',
+
+        # try to submit the form (knowing that the date is missing)
+        res2 = form.submit(u'submit', status=200)
+        form2 = res2.form
+        form2['date_of_birth'] = u'1999-09-19'
+        res3 = form2.submit(u'submit', status=302)
+        res4 = res3.follow()
+        #print res4
+        self.assertTrue('Details for Member Application #2' in res4.body)
+
+        # more asserts
+        self.assertTrue('SomeFirstname' in res4.body)
+        self.assertTrue('SomeLastname' in res4.body)
+        self.assertTrue('some@shri.de' in res4.body)
+        self.assertTrue('addr one' in res4.body)
+        self.assertTrue('addr two' in res4.body)
+        self.assertTrue('12345' in res4.body)
+        self.assertTrue('' in res4.body)
+        self.assertTrue('DE' in res4.body)
+        self.assertTrue('normal' in res4.body)
+        self.assertTrue('23' in res4.body)
+
+        # no member with id=1 in DB
+        res = self.testapp.get('/new_member', status=200)
+
+        form = res.form
+        #print form.fields
+        form['firstname'] = u'SomeFirstname',
+        form['lastname'] = u'SomeLastname',
+        form['email'] = u'some@shri.de',
+        form['address1'] = u"addr one",
+        form['address2'] = u"addr two",
+        form['postcode'] = u"12345",
+        form['city'] = u"Footown Meeh",
+        form['country'].value__set(u"DE"),
+        form['_LOCALE_'] = u"DE",
+        form['date_of_birth'] = date.today(),
+        #form['email_is_confirmed=False,
+        #email_confirm_code=u'ABCDEFGFOO',
+        #password=u'arandompassword',
+        #date_of_submission=date.today(),
+        #form['membership_type'] = u'normal',
+        form['deformField14'].value__set(u'normal')
+        #form['other_colsoc'] = (u'no',),
+        form['other_colsoc'].value__set(u'no')
+
+        form['name_of_colsoc'] = u"GEMA",
+        form['num_shares'] = u'23',
+
+        # try to submit the form (knowing that the date is missing)
+        res2 = form.submit(u'submit', status=200)
+        form2 = res2.form
+        form2['date_of_birth'] = u'1999-09-19'
+
+        res3 = form2.submit(u'submit', status=302)
+        res4 = res3.follow()
+        #print res4
+        self.assertTrue('Details for Member Application #3' in res4.body)
+
+        # more asserts
+        self.assertTrue('SomeFirstname' in res4.body)
+        self.assertTrue('SomeLastname' in res4.body)
+        self.assertTrue('some@shri.de' in res4.body)
+        self.assertTrue('addr one' in res4.body)
+        self.assertTrue('addr two' in res4.body)
+        self.assertTrue('12345' in res4.body)
+        self.assertTrue('' in res4.body)
+        self.assertTrue('DE' in res4.body)
+        self.assertTrue('normal' in res4.body)
+        self.assertTrue('23' in res4.body)
+
+        # check the number of entries in the DB
+        num = C3sMember.get_number()
+        #print num
+        self.assertTrue(num == 3)
