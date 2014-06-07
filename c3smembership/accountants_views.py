e9c6a1ff1544cf48671bb8eb5c3055c79a8083a7
Christoph Scheid <c@shri.de>
2014-06-07 02:28:50 +0200

    adds a view to import data from a backup (or export/csv) pertaining original ids

--- c3smembership/accountants_views.py
+++ c3smembership/accountants_views.py
@@ -373,6 +373,187 @@ def import_db(request):
     #             }
 
 
+@view_config(renderer='templates/import.pt',
+             permission='manage',
+             route_name='import_with_ids')
+def import_db_with_ids(request):
+    """
+    import the contents of import.csv to the database
+    and retain the given ids
+    """
+    try:  # check if the file exists
+        with open('import/import.csv', 'r') as f:
+            # store contents in tempfile
+            content = tempfile.NamedTemporaryFile()
+            content.write(f.read())
+            content.seek(0)  # rewind to beginning
+
+    except IOError, ioe:
+        print ioe
+        return {'message': "file not found.",
+                #        'codes': ''
+                }
+    # reader for CSV files
+    r = unicodecsv.reader(content.file, delimiter=';',
+                          encoding='utf-8',
+                          quoting=unicodecsv.QUOTE_ALL
+                          )
+    header = r.next()  # first line is the header.
+    #print("the header: %s" % header)
+    # check it for compatibility
+    try:
+        expected_header = [
+            u'id',  # n-1 ;-)
+            u'firstname', u'lastname', u'email',  # 0, 1, 2
+            u'password', u'last_password_change',  # 3, 4
+            u'address1', u'address2', u'postcode', u'city', u'country',  # 5-9
+            u'locale', u'date_of_birth',  # 10, 11
+            u'email_is_confirmed', u'email_confirm_code',  # 12, 13
+            u'num_shares', u'date_of_submission',  # 14, 15
+            u'membership_type',  # 16
+            u'member_of_colsoc', u'name_of_colsoc',  # 17, 18
+            u'signature_received', u'signature_received_date',  # 19, 20
+            u'payment_received', u'payment_received_date',  # 21, 22
+            u'signature_confirmed', u'signature_confirmed_date',  # 23, 24
+            u'payment_confirmed', u'payment_confirmed_date',  # 25, 26
+            u'accountant_comment'  # 27
+        ]
+        assert header == expected_header
+    except AssertionError, ae:
+        print ae
+        print "the header of the CSV does not match what we expect"
+        print "expected:"
+        print expected_header
+        print "got:"
+        print header
+        return {'message': "header fields mismatch. NOT importing",
+                #'codes': _codes,
+                }
+
+    # remember the codes imported
+    # _codes = []
+
+    # count the datasets
+    counter = 0
+
+    while True:
+        # import pdb
+        # pdb.set_trace()
+        try:
+            row = r.next()
+        except:
+            break
+        counter += 1
+        import_member = C3sMember(
+            firstname=row[1],
+            lastname=row[2],
+            email=row[3],
+            password=row[4],
+            address1=row[6],
+            address2=row[7],
+            postcode=row[8],
+            city=row[9],
+            country=row[10],
+            locale=row[11],
+            date_of_birth=datetime.strptime(row[12], '%Y-%m-%d'),
+            # (1970, 1, 1)
+            email_is_confirmed=True if (row[13] == 'True') else False,
+            email_confirm_code=row[14],
+            date_of_submission=row[16],
+            membership_type=row[17],
+            member_of_colsoc=True if (row[18] == 'True') else False,
+            name_of_colsoc=row[19],
+            num_shares=row[15],
+        )
+        import_member.last_password_change = datetime.strptime(
+            row[5], '%Y-%m-%d %H:%M:%S.%f')
+        import_member.date_of_submission = datetime.strptime(
+            row[16], '%Y-%m-%d %H:%M:%S.%f')
+        import_member.signature_received = True if (
+            row[20] == 'True') else False
+        try:
+            import_member.signature_received_date = datetime.strptime(
+                row[21], '%Y-%m-%d %H:%M:%S.%f')
+        except:
+            import_member.signature_received_date = datetime.strptime(
+                row[21], '%Y-%m-%d %H:%M:%S')
+        import_member.payment_received = True if (row[21] == 'True') else False
+        try:
+            import_member.payment_received_date = datetime.strptime(
+                row[23], '%Y-%m-%d %H:%M:%S')
+        except:
+            import_member.payment_received_date = datetime.strptime(
+                row[23], '%Y-%m-%d %H:%M:%S.%f')
+        import_member.signature_confirmed = True if (
+            row[24] == 'True') else False
+        try:
+            import_member.signature_confirmed_date = datetime.strptime(
+                row[25], '%Y-%m-%d %H:%M:%S')
+        except:
+            import_member.signature_confirmed_date = datetime.strptime(
+                row[25], '%Y-%m-%d %H:%M:%S.%f')
+
+        import_member.payment_confirmed = True if (
+            'True' in row[26]) else False
+        try:
+            import_member.payment_confirmed_date = datetime.strptime(
+                row[27], '%Y-%m-%d %H:%M:%S')
+        except:
+            import_member.payment_confirmed_date = datetime.strptime(
+                row[27], '%Y-%m-%d %H:%M:%S.%f')
+
+        import_member.accountant_comment = row[28]
+        #print('importing %s now...' % counter)
+        from types import NoneType
+        try:
+            _id = row[0]
+            print("the id we want to import to: %s" % _id)
+            # check if the id is present in the DB. if yes: goto fail
+            _mem = C3sMember.get_by_id(_id)  # load from id
+            if isinstance(_mem, NoneType):  # check if id is taken
+                import_member.id = _id  # set id as specified by import
+        except:
+            # well, we found an entry, so we just add a new id at the end
+            print "this id seems to have been taken."
+            return {'message': "header fields mismatch. NOT importing"}
+            #pass
+
+        try:
+            dbsession = DBSession
+            dbsession.add(import_member)
+            dbsession.flush()
+            log.info(
+                "%s imported dataset %s" % (
+                    authenticated_userid(request),
+                    import_member.email_confirm_code))
+            #print('done with %s!' % counter)
+        except ResourceClosedError, rce:
+            # XXX can't catch this exception,
+            # because it happens somwhere else, later, deeper !?!
+            print "transaction was aborted/resource closed"
+            print rce
+            return {'message': "tried import of dataset(s) with existing confirm code. ABORTED!"}
+        except IntegrityError, ie:
+            print "integrity error"
+            dbsession.rollback()
+            print ie
+            if 'column email_confirm_code is not unique' in ie.message:
+                print("import of dataset %s failed, because the confirmation"
+                      "code already existed" % counter)
+                return {'message': "tried import of dataset(s) with existing confirm code. ABORTED!"}
+        except StopIteration, si:
+            print "stop iteration reached"
+            print si
+            return {'message': "file found, StopIteration reached."}
+        #except:
+        #    print "passing"
+        #    pass
+
+    #print("done with all import steps, successful or not!")
+    return HTTPFound(
+        request.route_url('dashboard_only'))
+
+
 @view_config(renderer='csv',
              permission='manage',
              route_name='export_all')
