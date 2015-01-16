df42d41bec26a6ec502b7cb07f0c8a3bb51e07e6
Markus Lorenz <mrks@lrnz.name>
2015-01-16 22:37:18 +0100

    Added branch name and branch url to git_tools. Changed git_tools to use subprocess.Popen instead of check_output.

--- c3smembership/git_tools.py
+++ c3smembership/git_tools.py
@@ -8,26 +8,48 @@ class GitTools(object):
     """Provides methods to access git information."""
 
     @classmethod
+    def __execute_shell_command(cls, command):
+        """Encapsulates the execution of a shell command.
+
+        Performs the execution of the shell command and handles the result.
+        If an error occurred, the result will be None. Otherwise the
+        strout value will be returned as a stripped string.
+        """
+        try:
+            git = subprocess.Popen(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True,
+                universal_newlines=True)
+        except Exception:
+            # catch any exception which might be raised by the child process
+            return None
+        stdout_value, stderr_value = git.communicate()
+        if stderr_value != '' or len(stdout_value) < 1:
+            return None
+        return stdout_value.strip()
+
+
+    @classmethod
     def get_commit_hash(cls):
         """Returns the current git commit hash."""
-        git_commit_hash = None
-        try:
-            git_commit_hash = subprocess.check_output([
-                'git', 'rev-parse', 'HEAD'])
-        except subprocess.CalledProcessError:
-            pass
-        return git_commit_hash
+        return cls.__execute_shell_command(
+            'git rev-parse HEAD')
+
+
+    @classmethod
+    def get_branch(cls):
+        """Returns the current branch name."""
+        return cls.__execute_shell_command(
+            'git rev-parse --abbrev-ref HEAD')
+
 
     @classmethod
     def get_tag(cls):
-        """Returns the current tag."""
-        git_tag = None
-        try:
-            git_tag = subprocess.check_output(
-                ['git', 'describe', 'HEAD'])
-        except subprocess.CalledProcessError:
-            pass
-        return git_tag
+        """Returns the current tag name."""
+        return cls.__execute_shell_command('git describe HEAD')
+
 
     @classmethod
     def get_github_base_url(cls):
@@ -35,21 +57,21 @@ class GitTools(object):
 
         If the origin remote references Gibhut, the base URL is returned.
         """
-        github_base_url = None
-        try:
-            git_remote_url = subprocess.check_output(
-                ['git', 'config', '--get', 'remote.origin.url'])
-            match = re.search(
-                '^https://(?P<www>www.)?github.com/(?P<user>' + \
-                    '[A-Za-z0-9]+)/(?P<project>[A-Za-z0-9]+)(.git)?$',
-                git_remote_url)
-            if match is not None and match.groups():
-                github_base_url = 'https://github.com/{0}/{1}'.format(
-                    match.group('user'),
-                    match.group('project'))
-        except subprocess.CalledProcessError:
-            pass
-        return github_base_url
+        git_remote_url = cls.__execute_shell_command(
+            'git config --get remote.origin.url')
+        if git_remote_url is None:
+            return None
+        match = re.search(
+            '^https://(?P<www>www.)?github.com/(?P<user>' + \
+                '[A-Za-z0-9]+)/(?P<project>[A-Za-z0-9]+)(.git)?$',
+            git_remote_url)
+        if match is not None and match.groups():
+            return 'https://github.com/{0}/{1}'.format(
+                match.group('user'),
+                match.group('project'))
+        else:
+            return None
+
 
     @classmethod
     def get_github_commit_url(cls):
@@ -58,13 +80,28 @@ class GitTools(object):
         If the origin remote references Github, the URL to the current
         commit is returned.
         """
-        github_commit_url = None
         git_commit = cls.get_commit_hash()
         github_base_url = cls.get_github_base_url()
         if git_commit is not None and github_base_url is not None:
-            github_commit_url = '{0}/commit/{1}'.format(
+            return '{0}/commit/{1}'.format(
                 github_base_url,
                 git_commit)
-        return github_commit_url
+        else:
+            return None
 
 
+    @classmethod
+    def get_github_branch_url(cls):
+        """Return the Github URL to the current branch.
+
+        If the origin remote references Github, the URL to the current
+        branch is returned.
+        """
+        git_branch = cls.get_branch()
+        github_base_url = cls.get_github_base_url()
+        if git_branch is not None and github_base_url is not None:
+            return '{0}/tree/{1}'.format(
+                github_base_url,
+                git_branch)
+        else:
+            return None
--- c3smembership/tests/test_git_tools.py
+++ c3smembership/tests/test_git_tools.py
@@ -8,117 +8,239 @@ from c3smembership.git_tools import GitTools
 
 
 class TestGitTools(unittest.TestCase):
-    """Testing c3smembership.GitTools.
+    """Testing c3smembership.git_tools.GitTools.
 
-    The testing of c3smembership.GitTools is performed using the mock package
-    to mock subprocess.check_output.
+    The testing of c3smembership.git_tools.GitTools is performed using the
+    mock package to mock subprocess.
     """
 
-    def __test_check_output_result(
-            self,
-            args,
-            method,
-            check_output_result,
-            method_expected_output):
-        """Mock subprocess.check_output to return check_output_result and
-        call method.
 
-        The method subprocess.check_output is mocked and will return
-        check_output_result. The method is called invoking check_output.
-        Finally, checks are performed that check_output was called with
-        args and that method returned method_expected_output.
+    def __run_test(self, git_tools_method, std_pipe_values, \
+            expected_commands, expected_result):
+        """Provides an easy way to test GitTools methods encapsulating the
+        mocking.
+
+        GitTools methods usually use subprocess to execute git shell commands.
+        This method mocks the subprocess calls and performs checks.
         """
-        with mock.patch('c3smembership.git_tools.subprocess.check_output') \
-                as check_output:
-            check_output.return_value = check_output_result
-            method_output = method()
-            check_output.assert_called_once_with(args)
-            self.assertEqual(method_output, method_expected_output)
+        with mock.patch('c3smembership.git_tools.subprocess') as spmock:
+            spmock.Popen.return_value.communicate.side_effect = \
+                std_pipe_values
+            spmock.PIPE = subprocess.PIPE
+            method_result = git_tools_method()
+            for expected_command in expected_commands:
+                spmock.Popen.assert_any_call(
+                    expected_command,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    shell=True,
+                    universal_newlines=True)
+            self.assertEqual(len(expected_commands), spmock.Popen.call_count)
+            self.assertEqual(method_result, expected_result)
 
-    def __test_check_output_exception(
-            self,
-            args,
-            method,
-            method_expected_output):
-        """Mock subprocess.check_output to raise CalledProcessError
 
-        The method subprocess.check_output is mocked and will raise a
-        CalledProcessError exception. The method is called invoking
-        check_output. Finally, checks are performed that check_output was
-        called with args and that method returned method_expected_output.
+    def __run_test_cases(self, git_tools_method, test_cases, expected_commands):
+        """Runs a set of defined test cases for testing git_tools methods.
+
+        The git_tools_method is tested with each of the test_cases defining
+        mock values for stdout and strerr values, expected result values and
+        expected git commands to be executed.
         """
-        exception = subprocess.CalledProcessError(1, '', None)
-        with mock.patch('c3smembership.git_tools.subprocess.check_output', \
-                side_effect=exception) as check_output:
-            method_output = method()
-            check_output.assert_called_once_with(args)
-            self.assertEqual(method_output, method_expected_output)
+        for test_case in test_cases:
+            self.__run_test(
+                git_tools_method=git_tools_method,
+                expected_commands=expected_commands,
+                std_pipe_values=test_case['std_pipe_values'],
+                expected_result=test_case['expected_result'])
+
 
     def test_get_commit_hash(self):
-        """Test method c3smembership.GitTools.get_commit_hash().
+        """Test method GitTools.get_commit_hash().
 
-        Test for success and failure. Mocking is used to simulate behaviour
-        of subprocess.check_output.
+        Test for success and failure. Mocking is used to simulate the
+        behaviour of command line git.
         """
-        args = ['git', 'rev-parse', 'HEAD']
-        check_output_return_value = '0123456789abcdefghijklmnopqrstuvwxyz'
-        # test success
-        self.__test_check_output_result(
-            args,
+        test_cases = [
+            # test success
+            {
+                'std_pipe_values': [(
+                    '0123456789abcdefghijklmnopqrstuvwxyz\n',
+                    '')],
+                'expected_result': '0123456789abcdefghijklmnopqrstuvwxyz',
+            },
+            # test failure
+            {
+                'std_pipe_values': [('', 'fatal')],
+                'expected_result': None
+            },
+        ]
+        self.__run_test_cases(
             GitTools.get_commit_hash,
-            check_output_return_value,
-            check_output_return_value)
-        # test failure
-        self.__test_check_output_exception(
-            args,
-            GitTools.get_commit_hash,
-            None)
+            test_cases,
+            expected_commands=['git rev-parse HEAD'])
+
 
     def test_get_tag(self):
-        """Test method c3smembership.GitTools.get_tag().
+        """Test method GitTools.get_tag().
 
-        Test for success and failure. Mocking is used to simulate behaviour
-        of subprocess.check_output.
+        Test for success and failure. Mocking is used to simulate the
+        behaviour of command line git.
         """
-        args = ['git', 'describe', 'HEAD']
-        check_output_return_value = 'v1.2.3-14-g0123456'
-        # test success
-        self.__test_check_output_result(
-            args,
+        test_cases = [
+            # test success
+            {
+                'std_pipe_values': [('v1.2.3-14-g0123456\n', '')],
+                'expected_result': 'v1.2.3-14-g0123456',
+            },
+            # test failure
+            {
+                'std_pipe_values': [('', 'fatal')],
+                'expected_result': None
+            },
+        ]
+        self.__run_test_cases(
             GitTools.get_tag,
-            check_output_return_value,
-            check_output_return_value)
-        # test failure
-        self.__test_check_output_exception(
-            args,
-            GitTools.get_tag,
-            None)
+            test_cases,
+            expected_commands=['git describe HEAD'])
+
 
     def test_get_github_base_url(self):
-        """Test method c3smembership.GitTools.get_github_base_url().
+        """Test method GitTools.get_github_base_url().
 
-        Test for success and failure. Mocking is used to simulate behaviour
-        of subprocess.check_output.
+        Test for success and failure. Mocking is used to simulate the
+        behaviour of command line git.
         """
-        args = ['git', 'config', '--get', 'remote.origin.url']
-        expected_return_value = 'https://github.com/user/test123456789'
-
+        test_cases = []
         # test success
-        check_output_return_values = [
-            'https://github.com/user/test123456789',
-            'https://github.com/user/test123456789.git',
-            'https://www.github.com/user/test123456789',
-            'https://www.github.com/user/test123456789.git',
+        stdout_values = [
+            'https://github.com/user/test123456789\n',
+            'https://github.com/user/test123456789.git\n',
+            'https://www.github.com/user/test123456789\n',
+            'https://www.github.com/user/test123456789.git\n',
         ]
-        for check_output_return_value in check_output_return_values:
-            self.__test_check_output_result(
-                args,
-                GitTools.get_github_base_url,
-                check_output_return_value,
-                expected_return_value)
+        for stdout_value in stdout_values:
+            test_cases.append({
+                'std_pipe_values': [(stdout_value, '')],
+                'expected_result': 'https://github.com/user/test123456789'
+            })
         # test failure
-        self.__test_check_output_exception(
-            args,
+        test_cases.append({
+            'std_pipe_values': [('', 'fatal')],
+            'expected_result': None
+        })
+        self.__run_test_cases(
             GitTools.get_github_base_url,
-            None)
+            test_cases,
+            expected_commands=['git config --get remote.origin.url'])
+
+
+    def test_git_branch(self):
+        """Test method GitTools.git_branch().
+
+        Test for success and failure. Mocking is used to simulate the
+        behaviour of command line git.
+        """
+        test_cases = [
+            # test success
+            {
+                'std_pipe_values': [('testbranch\n', '')],
+                'expected_result': 'testbranch',
+            },
+            # test failure
+            {
+                'std_pipe_values': [('', 'fatal')],
+                'expected_result': None
+            },
+        ]
+        self.__run_test_cases(
+            GitTools.get_branch,
+            test_cases,
+            expected_commands=['git rev-parse --abbrev-ref HEAD'])
+
+
+    def test_get_github_commit_url(self):
+        """Test method GitTools.get_github_commit_url().
+
+        Test for success and failure. Mocking is used to simulate the
+        behaviour of command line git.
+        """
+        test_cases = [
+            # test success
+            {
+                'std_pipe_values': [
+                    # commit result
+                    ('0123456789abcdefghijklmnopqrstuvwxyz\n', ''),
+                    # git base url result
+                    ('https://github.com/user/test123456789\n', '')
+                ],
+                'expected_result': 'https://github.com/user/' + \
+                    'test123456789/commit/0123456789abcdefgh' + \
+                    'ijklmnopqrstuvwxyz'
+            },
+            # test failure: no commit
+            {
+                'std_pipe_values': [
+                    ('', 'fatal'),
+                    ('https://github.com/user/test123456789\n', '')
+                ],
+                'expected_result': None
+            },
+            # test failure: no origin remote
+            {
+                'std_pipe_values': [
+                    ('0123456789abcdefghijklmnopqrstuvwxyz\n', ''),
+                    ('', 'fatal')
+                ],
+                'expected_result': None
+            },
+        ]
+        self.__run_test_cases(
+            GitTools.get_github_commit_url,
+            test_cases,
+            expected_commands=[
+                'git rev-parse HEAD',
+                'git config --get remote.origin.url'])
+
+
+    def test_get_github_branch_url(self):
+        """Test method GitTools.get_github_branch_url().
+
+        Test for success and failure. Mocking is used to simulate the
+        behaviour of command line git.
+        """
+        test_cases = [
+            # test success
+            {
+                'std_pipe_values': [
+                    # branch result
+                    ('testbranch\n', ''),
+                    # git base url result
+                    ('https://github.com/user/test123456789\n', '')
+                ],
+                'expected_result': 'https://github.com/user/' + \
+                    'test123456789/tree/testbranch'
+            },
+            # test failure: no commit
+            {
+                'std_pipe_values': [
+                    ('', 'fatal'),
+                    ('https://github.com/user/test123456789\n', '')
+                ],
+                'expected_result': None
+            },
+            # test failure: no origin remote
+            {
+                'std_pipe_values': [
+                    ('testbranch', ''),
+                    ('', 'fatal')
+                ],
+                'expected_result': None
+            },
+        ]
+        self.__run_test_cases(
+            GitTools.get_github_branch_url,
+            test_cases,
+            expected_commands=[
+                'git rev-parse --abbrev-ref HEAD',
+                'git config --get remote.origin.url'])
 
