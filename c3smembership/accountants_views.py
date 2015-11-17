buffstop/PepCoopMembership

commit f09616c3552e3813177b14aa7cbe6b5ebee8b06a

Pylint code cleanup. No functionality changes.

c3smembership/accountants_views.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-import tempfile
-import unicodecsv
-
 from c3smembership.models import (
     C3sMember,
     C3sStaff,
-    DBSession,
 )
 from c3smembership.utils import generate_pdf
 from c3smembership.mail_utils import (
@@ -38,10 +34,6 @@ from pyramid_mailer.message import Message
 from pyramid.url import route_url
 from translationstring import TranslationStringFactory
 from datetime import datetime
-from sqlalchemy.exc import (
-    IntegrityError,
-    ResourceClosedError,
-)
 import math
 
 deform_templates = resource_filename('deform', 'templates')
@@ -58,6 +50,7 @@ def translator(term):
 my_template_dir = resource_filename('c3smembership', 'templates/')
 deform_template_dir = resource_filename('deform', 'templates/')
 
+# the zpt_renderer is referred to within the demo.ini file by dotted name
 zpt_renderer = deform.ZPTRendererFactory(
     [
         my_template_dir,
@@ -65,7 +58,6 @@ zpt_renderer = deform.ZPTRendererFactory(
     ],
     translator=translator,
 )
-# the zpt_renderer above is referred to within the demo.ini file by dotted name
 
 DEBUG = False
 LOGGING = True
@@ -83,13 +75,11 @@ def accountants_login(request):
     if a person is already logged in, she is forwarded to the dashboard
     """
     logged_in = authenticated_userid(request)
-    #print("authenticated_userid: " + str(logged_in))
 
-    log.info("login by %s" % logged_in)
+    log.info("login by {0}".format(logged_in))
 
-    if logged_in is not None:  # if user is already authenticated
-        return HTTPFound(  # redirect her to the dashboard
-            request.route_url('dashboard_only'))
+    if logged_in is not None:
+        return HTTPFound(request.route_url('dashboard_only'))
 
     class AccountantLogin(colander.MappingSchema):
         """
@@ -98,15 +88,13 @@ def accountants_login(request):
         login = colander.SchemaNode(
             colander.String(),
             title=_(u"login"),
-            oid="login",
-        )
+            oid="login")
         password = colander.SchemaNode(
             colander.String(),
             validator=colander.Length(min=5, max=100),
             widget=deform.widget.PasswordWidget(size=20),
             title=_(u"password"),
-            oid="password",
-        )
+            oid="password")
 
     schema = AccountantLogin()
 
@@ -116,13 +104,10 @@ def accountants_login(request):
             deform.Button('submit', _(u'Submit')),
             deform.Button('reset', _(u'Reset'))
         ],
-        #use_ajax=True,
-        #renderer=zpt_renderer
     )
 
     # if the form has been used and SUBMITTED, check contents
     if 'submit' in request.POST:
-        #print("the form was submitted")
         controls = request.POST.items()
         try:
             appstruct = form.validate(controls)
@@ -145,18 +130,18 @@ def accountants_login(request):
         except AttributeError:  # pragma: no cover
             checked = False
         if checked:
-            log.info("password check for %s: good!" % login)
+            log.info("password check for {0}: good!".format(login))
             headers = remember(request, login)
-            log.info("logging in %s" % login)
-            return HTTPFound(  # redirect to accountants dashboard
-                location=route_url(  # after successful login
+            log.info("logging in {0}".format(login))
+            return HTTPFound(
+                location=route_url(
                     'dashboard_only',
                     request=request),
                 headers=headers)
         else:
             log.info("password check: failed for %s." % login)
     else:
-        request.session.pop('message_above_form')  # remove message fr. session
+        request.session.pop('message_above_form')
 
     html = form.render()
     return {'form': html, }
@@ -170,85 +155,48 @@ def accountants_desk(request):
     This view lets accountants view applications and set their status:
     has their signature arrived? how about the payment?
     """
-    #_number_of_datasets = C3sMember.get_num_nonmember_listing()
     _number_of_datasets = C3sMember.nonmember_listing_count()
-    #print "number of datasets: {}".format(_number_of_datasets)
-    #C3sMember.get_num_non_accepted()
-    try:  # check if page number, orderby and order were supplied with the URL
+    try:
         _page_to_show = int(request.matchdict['number'])
         _order_by = request.matchdict['orderby']
         _order = request.matchdict['order']
     except:
-        #print("Using default values")
         _page_to_show = 0
         _order_by = 'id'
         _order = 'asc'
 
-    # '''
-    # there used to be a form on this page allowing input of
-    # one 'code_to_show' in case you wanted to see it
-    # and be redirected there
-    # '''
-    # # check for input from "find dataset by confirm code" form
-    # if 'code_to_show' in request.POST:
-    #     try:
-    #         _code = request.POST['code_to_show']
-    #         log.info(
-    #             "%s searched for code %s" % (
-    #                 authenticated_userid(request), _code))
-    #         _entry = C3sMember.get_by_code(_code)
-
-    #         return HTTPFound(
-    #             location=request.route_url(
-    #                 'detail',
-    #                 memberid=_entry.id)
-    #         )
-    #     except:
-    #         pass
-
-    """
-    num_display determines how many items are to be shown on one page
-    """
     if 'num_to_show' in request.POST:
         try:
             _num = int(request.POST['num_to_show'])
             if isinstance(_num, type(1)):
                 num_display = _num
         except:
-            # choose default
             num_display = 20
     elif 'num_display' in request.cookies:
-        #print("found it in cookie")
         num_display = int(request.cookies['num_display'])
     else:
-        #print("setting default")
         num_display = request.registry.settings[
             'c3smembership.dashboard_number']
 
-    """
-    base_offset helps us to minimize impact on the database
-    when querying for results.
-    we can choose just those results we need for the page to show
-    """
+    # base_offset helps us to minimize impact on the database
+    # when querying for results.
+    # we can choose just those results we need for the page to show
     base_offset = int(_page_to_show) * int(num_display)
 
-    '''
-    load the list from the database
-    '''
-    # get data sets from DB
+    # load the list from the database
     _members = C3sMember.nonmember_listing(
         _order_by, how_many=num_display, offset=base_offset, order=_order)
-    #print "DEBUG: number of items in _members: {}".format(len(_members))
 
     # calculate next-previous-navi
-    next_page = (int(_page_to_show) + 1)
-    if (int(_page_to_show) > 0):
+    next_page = int(_page_to_show) + 1
+    if int(_page_to_show) > 0:
         previous_page = int(_page_to_show) - 1
     else:
         previous_page = int(_page_to_show)
     _last_page = int(math.ceil(_number_of_datasets / int(num_display)))
     if next_page > _last_page:
         next_page = _last_page
+
     # store info about current page in cookie
     request.response.set_cookie('on_page', value=str(_page_to_show))
     request.response.set_cookie('num_display', value=str(num_display))
@@ -259,19 +207,20 @@ def accountants_desk(request):
     if 'message' in request.GET:
         _message = request.GET['message']
 
-    return {'_number_of_datasets': _number_of_datasets,
-            'members': _members,
-            'num_display': num_display,
-            'next': next_page,
-            'previous': previous_page,
-            'current': _page_to_show,
-            'orderby': _order_by,
-            'order': _order,
-            'message': _message,
-            'last_page': _last_page,
-            'is_last_page': _page_to_show == _last_page,
-            'is_first_page': _page_to_show == 0,
-            }
+    return {
+        '_number_of_datasets': _number_of_datasets,
+        'members': _members,
+        'num_display': num_display,
+        'next': next_page,
+        'previous': previous_page,
+        'current': _page_to_show,
+        'orderby': _order_by,
+        'order': _order,
+        'message': _message,
+        'last_page': _last_page,
+        'is_last_page': _page_to_show == _last_page,
+        'is_first_page': _page_to_show == 0,
+    }
 
 
 @view_config(permission='manage',
@@ -282,7 +231,6 @@ def switch_sig(request):
     has their signature arrived?
     """
     memberid = request.matchdict['memberid']
-    #log.info("the id: %s" % memberid)
 
     # store the dashboard page the admin came from
     dashboard_page = request.cookies['on_page']
@@ -335,7 +283,8 @@ def delete_entry(request):
     return HTTPFound(
         request.route_url(
             'dashboard_only',
-            _query={'message': 'Member with id {0} was deleted.'.format(
+            _query={
+                'message': 'Member with id {0} was deleted.'.format(
                     memberid)}
         ) + '#member_' + str(_member.id)
     )
@@ -385,26 +334,14 @@ def member_detail(request):
     has their signature arrived? how about the payment?
     """
     logged_in = authenticated_userid(request)
-    #log.info("detail view.................................................")
-    #print("---- authenticated_userid: " + str(logged_in))
-
-    # this following stanza is overridden by the views permission settings
-    #if logged_in is None:  # not authenticated???
-    #    return HTTPFound(  # go back to login!!!
-    #        location=route_url(
-    #            'login',
-    #            request=request),
-    #    )
-
     memberid = request.matchdict['memberid']
     log.info("member details of id %s checked by %s" % (
         memberid, logged_in))
 
     _member = C3sMember.get_by_id(memberid)
 
-    #print(_member)
-    if _member is None:  # that memberid did not produce good results
-        return HTTPFound(  # back to base
+    if _member is None:
+        return HTTPFound(
             request.route_url('dashboard_only'))
 
     class ChangeDetails(colander.MappingSchema):
@@ -438,12 +375,7 @@ def member_detail(request):
             appstruct = form.validate(controls)
         except ValidationFailure, e:  # pragma: no cover
             log.info(e)
-            #print("the appstruct from the form: %s \n") % appstruct
-            #for thing in appstruct:
-            #    print("the thing: %s") % thing
-            #    print("type: %s") % type(thing)
             print(e)
-            #message.append(
             request.session.flash(
                 _(u"Please note: There were errors, "
                   "please check the form below."),
@@ -452,7 +384,6 @@ def member_detail(request):
             return{'form': e.render()}
 
         # change info about member in database
-
         test1 = (  # changed value through form (different from db)?
             appstruct['signature_received'] == _member.signature_received)
         if not test1:
@@ -483,7 +414,6 @@ def member_detail(request):
             'signature_received': _member.signature_received,
             'payment_received': _member.payment_received}
         form.set_appstruct(appstruct)
-        #print("the appstruct: %s") % appstruct
     html = form.render()
 
     return {'member': _member,
@@ -499,8 +429,10 @@ def logout_view(request):
     request.session.invalidate()
     request.session.flash(u'Logged out successfully.')
     headers = forget(request)
-    return HTTPFound(location=route_url('login', request),
-                     headers=headers)
+    return HTTPFound(
+        location=route_url('login', request),
+        headers=headers
+    )
 
 
 @view_config(permission='manage',
@@ -512,9 +444,8 @@ def regenerate_pdf(request):
     _code = request.matchdict['code']
     _member = C3sMember.get_by_code(_code)
 
-    if _member is None:  # that memberid did not produce good results
-        return HTTPFound(  # back to base
-            request.route_url('dashboard_only'))
+    if _member is None:
+        return HTTPFound(request.route_url('dashboard_only'))
     _appstruct = {
         'firstname': _member.firstname,
         'lastname': _member.lastname,
@@ -547,7 +478,8 @@ def mail_signature_confirmation(request):
     _id = request.matchdict['memberid']
     _member = C3sMember.get_by_id(_id)
     if _member.locale == 'de':
-        _subject = u'[C3S AFM] Wir haben Deine Unterschrift erhalten. Dankeschön!'
+        _subject = u'[C3S AFM] Wir haben Deine Unterschrift ' + \
+            'erhalten. Dankeschön!'
     else:
         _subject = u'[C3S AFM] We have received your signature. Thanks!'
 
@@ -557,16 +489,15 @@ def mail_signature_confirmation(request):
         recipients=[_member.email],
         body=make_signature_confirmation_emailbody(_member)
     )
-    #print(message.body)
     mailer = get_mailer(request)
     mailer.send(message)
     _member.signature_confirmed = True
     _member.signature_confirmed_date = datetime.now()
-    return HTTPFound(request.route_url('dashboard',
-                                       number=request.cookies['on_page'],
-                                       order=request.cookies['order'],
-                                       orderby=request.cookies['orderby'])
-                     )
+    return HTTPFound(request.route_url(
+        'dashboard',
+        number=request.cookies['on_page'],
+        order=request.cookies['order'],
+        orderby=request.cookies['orderby']))
 
 
 @view_config(permission='manage',
@@ -590,17 +521,15 @@ def mail_payment_confirmation(request):
         recipients=[_member.email],
         body=make_payment_confirmation_emailbody(_member)
     )
-    #print(message.body)
     mailer = get_mailer(request)
     mailer.send(message)
     _member.payment_confirmed = True
     _member.payment_confirmed_date = datetime.now()
-    return HTTPFound(request.route_url('dashboard',
-                                       number=request.cookies['on_page'],
-                                       order=request.cookies['order'],
-                                       orderby=request.cookies['orderby'],
-                                       )
-                     )
+    return HTTPFound(request.route_url(
+        'dashboard',
+        number=request.cookies['on_page'],
+        order=request.cookies['order'],
+        orderby=request.cookies['orderby']))
 
 
 @view_config(permission='manage',
@@ -624,23 +553,16 @@ def mail_signature_reminder(request):
                 order=request.cookies['order'],
                 orderby=request.cookies['orderby']))
 
-    # first reminder? second?
-    #if ((_member.sent_signature_reminder is None
-    #) or (    ):
-    #_first =
     message = Message(
-        subject=u"C3S: don't forget to send your form / Bitte Beitrittsformular einsenden",
+        subject=u"C3S: don't forget to send your form / Bitte " + \
+            'Beitrittsformular einsenden',
         sender='office@c3s.cc',
-        #bcc=[request.registry.settings['reminder_blindcopy']],
         recipients=[_member.email],
         body=make_signature_reminder_emailbody(_member)
     )
     mailer = get_mailer(request)
     mailer.send(message)
-    #print u"the mail: {}".format(message.body)
-    #import pdb
-    #pdb.set_trace()
-    try:  # if value is int
+    try:
         _member.sent_signature_reminder += 1
     except:  # pragma: no cover
         # if value was None (after migration of DB schema)
@@ -650,8 +572,7 @@ def mail_signature_reminder(request):
         'dashboard',
         number=request.cookies['on_page'],
         order=request.cookies['order'],
-        orderby=request.cookies['orderby']) + '#member_' + str(_member.id)
-    )
+        orderby=request.cookies['orderby']) + '#member_' + str(_member.id))
 
 
 @view_config(permission='manage',
@@ -665,9 +586,9 @@ def mail_payment_reminder(request):
     _member = C3sMember.get_by_id(_id)
 
     message = Message(
-        subject=u"C3S: don't forget to pay your shares / Bitte Anteile bezahlen",
+        subject=u"C3S: don't forget to pay your shares / Bitte Anteile " + \
+            "bezahlen",
         sender='office@c3s.cc',
-        #bcc=[request.registry.settings['reminder_blindcopy']],
         recipients=[_member.email],
         body=make_payment_reminder_emailbody(_member)
     )
@@ -683,37 +604,9 @@ def mail_payment_reminder(request):
         'dashboard',
         number=request.cookies['on_page'],
         order=request.cookies['order'],
-        orderby=request.cookies['orderby']) + '#member_' + str(_member.id)
-    )
+        orderby=request.cookies['orderby']) + '#member_' + str(_member.id))
 
 
-
-# @view_config(permission='manage',
-#              route_name='mail_pay_confirmation')
-# def mail_passwd_reset(request):
-#     """
-#     send a mail to member to reset her password
-#     """
-#     _id = request.matchdict['memberid']
-#     _member = C3sMember.get_by_id(_id)
-
-#     message = Message(
-#         subject=_('[C3S AFM] Please reset your password!'),
-#         sender='yes@c3s.cc',
-#         recipients=[_member.email],
-#         body=make_password_reset_emailbody(_member)
-#     )
-#     #print(message.body)
-#     mailer = get_mailer(request)
-#     mailer.send(message)
-#     _member.password XXX = True
-#     _member. XXX = datetime.now()
-#     return HTTPFound(request.route_url('dashboard',
-#                                        number=request.cookies['on_page'],
-#                                        order=request.cookies['order'],
-#                                        orderby=request.cookies['orderby'],
-#                                        )
-#                      )
 
 @view_config(permission='manage', route_name='dashboard_only')
 def dashboard_only(request):
@@ -736,4 +629,6 @@ def dashboard_only(request):
         request.route_url(
             'dashboard',
             number=_number, orderby=_order_by,
-            order=_order, _query=request.GET))
+            order=_order, _query=request.GET
+        )
+    )

commit 790b2c5ef40549172bf0b6a28142de141cb08af9

fix encoding bug
introduced in commit f09616c3552e3813177b14aa7cbe6b5ebee8b06a

c3smembership/accountants_views.py
@@ -542,7 +542,7 @@ def mail_signature_confirmation(request):
     _member = C3sMember.get_by_id(_id)
     if _member.locale == 'de':
         _subject = u'[C3S AFM] Wir haben Deine Unterschrift ' + \
-            'erhalten. Dankeschön!'
+                   u'erhalten. Dankeschön!'
     else:
         _subject = u'[C3S AFM] We have received your signature. Thanks!'
 