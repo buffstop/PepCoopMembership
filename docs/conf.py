b52d8c9c45defe39f68f35c8d0878b6ba14f9f12
Christoph Scheid <christoph.scheid@c3s.cc>
2016-02-11 18:59:45 +0100

    add missing conf.py changes for test documentation: sys.path needs additions

--- docs/conf.py
+++ docs/conf.py
@@ -21,12 +21,21 @@ import alabaster
 # relative to the documentation root, use os.path.abspath to make it absolute,
 # like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
+
+# as the docs folder is a subfolder of the c3smembership project,
+# neighboring the packages code,
+#  we have to add the packages path to sys.path so autodoc can do its work
 sys.path.insert(0, os.path.abspath('..'))
+# as the tests are not really part of the packages code
+# -- there is no __init__.py in the folders containing the tests --
+# we need to add those folders to sys.path so we can use autodoc with them:
+sys.path.append(os.path.abspath('../c3smembership/tests'))
+sys.path.append(os.path.abspath('../c3smembership/views/tests'))
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+# needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -37,6 +46,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.coverage',
     'sphinx.ext.graphviz',
+    'sphinx.ext.napoleon',
     'sphinxcontrib.plantuml',
 ]
 
@@ -59,7 +69,7 @@ numfig_format = {
 numfig_secnum_depth = 1
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+# source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
