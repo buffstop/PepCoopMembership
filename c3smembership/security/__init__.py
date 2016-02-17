eb0424a3d47967a40e669741f26ccc4bc92db93b
Christoph Scheid <christoph.scheid@c3s.cc>
2016-02-17 16:55:04 +0100

    better docstrings

--- c3smembership/security/__init__.py
+++ c3smembership/security/__init__.py
@@ -1,18 +1,39 @@
 from pyramid.security import (
     Allow,
-    #Deny,
+    # Deny,
 )
 from pyramid.security import ALL_PERMISSIONS
 
 
 def groupfinder(userid, request):
+    """
+    Find Groups for a user.
+
+    Args:
+        userid: the current userid
+        request: the current request
+
+    Returns:
+        list: strings of group names
+    """
     user = request.user
     if user:
         return ['%s' % g for g in user.groups]
 
 
-### MAP GROUPS TO PERMISSIONS
+# ### MAP GROUPS TO PERMISSIONS
 class Root(object):
+    """
+    This is the Security Root object,
+    where groups are given certain permissions.
+
+    see also
+    http://docs.pylonsproject.org/projects/pyramid/en/latest/narr/security.html#assigning-acls-to-your-resource-objects
+
+    **__acl__**
+
+    * Access Control List (ACL) consisting of Access Control Entries (ACE)
+    """
     __acl__ = [
         (Allow, 'system.Everyone', 'view'),
         (Allow, 'group:staff', ALL_PERMISSIONS),
--- c3smembership/security/request.py
+++ c3smembership/security/request.py
@@ -1,4 +1,4 @@
-### Making A 'User Object' Available as a Request Attribute
+# ### Making A 'User Object' Available as a Request Attribute
 # docs.pylonsproject.org/projects/pyramid_cookbook/dev/authentication.html
 from pyramid.decorator import reify
 from pyramid.request import Request
@@ -9,14 +9,29 @@ from c3smembership.models import (
 
 
 class RequestWithUserAttribute(Request):
+    """
+    This class is used as replacement for the common Request class.
+
+    It adds an important feature for c3sMembership: a request.user attribute,
+    telling who is authenticated and using the app during this request.
+
+    This is useful to protect some views to be used by staff only.
+    """
     @reify
     def user(self):
+        """
+        The request.user attribute.
+
+        Returns:
+            * **id**, if user is known.
+            * **None**, if user is not known.
+        """
         userid = unauthenticated_userid(self)
-        #print "--- in RequestWithUserAttribute: userid = " + str(userid)
+        # print "--- in RequestWithUserAttribute: userid = " + str(userid)
         if userid is not None:
             # this should return None if the user doesn't exist
             # in the database
-            #return dbsession.query('users').filter(user.user_id == userid)
+            # return dbsession.query('users').filter(user.user_id == userid)
             return C3sStaff.check_user_or_None(userid)
         # else: userid == None
         return userid  # pragma: no cover
