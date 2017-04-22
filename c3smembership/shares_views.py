8181e0d8aef4d439b6fd84bfe9e15736c62065c8
Markus Lorenz <mrks@lrnz.name>
2017-04-22 15:06:41 +0200

    Code cleanup.

--- c3smembership/shares_views.py
+++ c3smembership/shares_views.py
@@ -1,24 +1,23 @@
 """
 This module holds functionality to administer a members shares.
 """
+
+import logging
+from types import NoneType
+
 import colander
 import deform
 from deform import ValidationFailure
 from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
-from types import NoneType
-from c3smembership.models import Shares
 
+from c3smembership.models import Shares
 from c3smembership.presentation.i18n import _
 from c3smembership.presentation.views.membership_listing import (
     get_memberhip_listing_redirect
 )
 
-LOGGING = True
-
-if LOGGING:  # pragma: no cover
-    import logging
-    log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 
 @view_config(renderer='templates/shares_detail.pt',
@@ -28,11 +27,11 @@ def shares_detail(request):
     '''
     Show details about a package of shares.
     '''
-    _s = Shares.get_by_id(request.matchdict['id'])
-    if isinstance(_s, NoneType):
+    share = Shares.get_by_id(request.matchdict['id'])
+    if isinstance(share, NoneType):
         # entry was not found in database
         request.session.flash(
-            "This shares id was not found in the database!",
+            'This shares id was not found in the database!',
             'message_to_staff'
         )
         return HTTPFound(
@@ -40,21 +39,19 @@ def shares_detail(request):
 
     # get shares owner if possible
     try:
-        _m_id = _s.members[0].id
-        _m_first = _s.members[0].firstname
-        _m_last = _s.members[0].lastname
-        # print('got it!')
-    except:
-        # print('failed!')
-        _m_id = 0
-        _m_first = 'Not'
-        _m_last = 'Found'
+        member_id = share.members[0].id
+        member_firstname = share.members[0].firstname
+        member_lastname = share.members[0].lastname
+    except IndexError:
+        member_id = 0
+        member_firstname = 'Not'
+        member_lastname = 'Found'
 
     return {
-        's': _s,  # the share
-        'm_id': _m_id,  # the owner
-        'm_first': _m_first,  # the owner
-        'm_last': _m_last,  # the owner
+        's': share,  # the share
+        'm_id': member_id,  # the owner
+        'm_first': member_firstname,  # the owner
+        'm_last': member_lastname,  # the owner
     }
 
 
@@ -65,23 +62,24 @@ def shares_edit(request):
     '''
     Edit details of a package of shares.
     '''
-    # print(request.matchdict['id'])
-    from c3smembership.models import Shares
     # load info from DB -- if possible
-    _s = Shares.get_by_id(request.matchdict['id'])
+    share = Shares.get_by_id(request.matchdict['id'])
 
-    if isinstance(_s, NoneType):
+    if isinstance(share, NoneType):
         # entry was not found in database
         return get_memberhip_listing_redirect(request)
     else:
         appstruct = {}
         appstruct = {
-            'number': _s.number,
-            'date_of_acquisition': _s.date_of_acquisition,
+            'number': share.number,
+            'date_of_acquisition': share.date_of_acquisition,
         }
 
     # construct a form
-    class Shares(colander.Schema):
+    class SharesSchema(colander.Schema):
+        """
+        Defines the colander schema for shares.
+        """
         number = colander.SchemaNode(
             colander.Integer(),
             title=_('Number of Shares'),
@@ -90,7 +88,7 @@ def shares_edit(request):
             colander.Date(),
             title=_('Date of Acquisition')
         )
-    schema = Shares()
+    schema = SharesSchema()
     form = deform.Form(
         schema,
         buttons=[deform.Button('submit', _(u'Submit'))],
@@ -102,40 +100,35 @@ def shares_edit(request):
         controls = request.POST.items()
         try:
             appstruct = form.validate(controls)
-            # print("the appstruct from the form: %s \n") % appstruct
-            # for thing in appstruct:
-            #     print("the thing: %s") % thing
-            #     print("type: %s") % type(thing)
 
-        except ValidationFailure, e:  # pragma: no cover
-            print(e)
+        except ValidationFailure, validation_failure:  # pragma: no cover
             request.session.flash(
-                _(u"Please note: There were errors, "
-                  "please check the form below."),
+                _(u'Please note: There were errors, '
+                  'please check the form below.'),
                 'message_above_form',
                 allow_duplicate=False)
-            return{'form': e.render()}
+            return{'form': validation_failure.render()}
 
         # if no error occurred, persist the changed values info in database
 
         test1 = (  # changed value through form (different from db)?
-            appstruct['number'] == _s.number)
+            appstruct['number'] == share.number)
         if not test1:
-            log.info(
-                "info about number of shares of %s changed by %s to %s" % (
-                    _s.id,
-                    request.user.login,
-                    appstruct['number']))
-            _s.number = appstruct['number']
+            LOG.info(
+                'info about number of shares of %s changed by %s to %s',
+                share.id,
+                request.user.login,
+                appstruct['number'])
+            share.number = appstruct['number']
         test2 = (  # changed value through form (different from db)?
-            appstruct['date_of_acquisition'] == _s.date_of_acquisition)
+            appstruct['date_of_acquisition'] == share.date_of_acquisition)
         if not test2:
-            log.info(
-                "info about date_of_acquisition of %s changed by %s to %s" % (
-                    _s.id,
-                    request.user.login,
-                    appstruct['date_of_acquisition']))
-            _s.date_of_acquisition = appstruct['date_of_acquisition']
+            LOG.info(
+                'info about date_of_acquisition of %s changed by %s to %s',
+                share.id,
+                request.user.login,
+                appstruct['date_of_acquisition'])
+            share.date_of_acquisition = appstruct['date_of_acquisition']
         # store appstruct in session
         # request.session['appstruct'] = appstruct
 
@@ -146,7 +139,7 @@ def shares_edit(request):
     html = form.render()
 
     return {
-        's': _s,
+        's': share,
         'form': html
     }
 
@@ -157,32 +150,31 @@ def shares_delete(request):
     '''
     Staff may delete a package of shares.
     '''
-    _id = request.matchdict['id']
-    from c3smembership.models import Shares
+    shares_id = request.matchdict['id']
     # load info from DB -- if possible
-    _s = Shares.get_by_id(_id)
+    share = Shares.get_by_id(shares_id)
 
-    if isinstance(_s, NoneType):
+    if isinstance(share, NoneType):
         # entry was not found in database
         request.session.flash(
-            "This shares package {} was not found in the DB.".format(_id),
+            'This shares package {} was not found in the DB.'.format(shares_id),
             'message_to_staff'
         )
         return HTTPFound(request.route_url('toolbox'))
 
-    # print("any members? {}".format(_s.members))
-    if (len(_s.members) > 0):
+    if len(share.members) > 0:
         # shares package is still owned
         request.session.flash(
-            "DID NOT DELETE! "
-            "This shares package {} still has a member owning it.".format(_id),
+            'DID NOT DELETE! '
+            'This shares package {} still has a member owning it.'.format(
+                shares_id),
             'message_to_staff'
         )
         return HTTPFound(request.route_url('toolbox'))
     else:
-        Shares.delete_by_id(_id)
+        Shares.delete_by_id(shares_id)
         request.session.flash(
-            "the shares package {} was deleted.".format(_id),
+            'the shares package {} was deleted.'.format(shares_id),
             'message_to_staff'
         )
         return HTTPFound(request.route_url('toolbox'))
